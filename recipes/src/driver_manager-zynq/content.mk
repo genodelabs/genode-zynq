0b543c64eabd5818d7b23ee47dd59e94a559e892
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2021-11-16 15:16:45 +0100

    depot: create drivers_fpga-zynq package
    The package combines the fpga_drv and driver_manager to dynamically load
    custom bitstreams and start the corresponding drivers.

---
+++ recipes/src/driver_manager-zynq/content.mk
@@ -0,0 +1,2 @@
+SRC_DIR = src/app/driver_manager/zynq
+include $(GENODE_DIR)/repos/base/recipes/src/content.inc
---
+++ recipes/src/fpga_drv/content.mk
@@ -0,0 +1,2 @@
+SRC_DIR = src/drivers/fpga
+include $(GENODE_DIR)/repos/base/recipes/src/content.inc
