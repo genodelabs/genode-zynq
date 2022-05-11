a7486e4c067741c801fc37785ef55eb432cb8bcf
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2022-05-11 15:04:40 +0200

    Adapt to changes of the generic platform_drv
    Especially address that the devices ROM consumed by the platform
    driver is now separated from the configuration by default.
    
    Ref genodelabs/genode#4386
    Ref genodelabs/genode#4491
    Ref genodelabs/genode#4496
    Ref genodelabs/genode#4497
    Ref genodelabs/genode#4502
    Ref genodelabs/genode#4503
    Ref genodelabs/genode#4504

--- src/drivers/platform/zynq/main.cc
+++ src/drivers/platform/zynq/main.cc
@@ -14,49 +14,45 @@
 #include <base/component.h>
 
 #include <slcr.h>
-#include <root.h>
+#include <common.h>
 
 namespace Driver { struct Main; };
 
 struct Driver::Main
 {
 	Env                  & _env;
-	Heap                   _heap        { _env.ram(), _env.rm() };
-	Sliced_heap            _sliced_heap { _env.ram(), _env.rm() };
-	Attached_rom_dataspace _config      { _env, "config" };
-	Device_model           _devices     { _heap };
+	Attached_rom_dataspace _config_rom     { _env, "config"        };
+	Common                 _common         { _env, _config_rom     };
+	Signal_handler<Main>   _config_handler { _env.ep(), *this,
+	                                         &Main::_handle_config };
 
 	/**
 	 * PS_CLK is either 33.33333Mhz or 50MHz. The latter is very rare though
 	 * and practically not in use, hence we stick to 33.33333Mhz.
 	 */
-	Fixed_clock _ps_clk { _devices.clocks(), "ps_clk",
+	Fixed_clock _ps_clk { _common.devices().clocks(), "ps_clk",
 	                      Clock::Rate { 33333333 } };
 
-	Slcr  _slcr { _env, _devices.clocks(), _ps_clk };
+	Slcr  _slcr { _env, _common.devices().clocks(), _ps_clk };
 
 	void _handle_config();
 
-	Signal_handler<Main> _config_handler { _env.ep(), *this,
-	                                       &Main::_handle_config };
-
-	Driver::Root _root { _env, _sliced_heap, _config, _devices };
-
-	Main(Genode::Env &env) : _env(env)
+	Main(Genode::Env & e)
+	: _env(e)
 	{
-		_devices.update(_config.xml());
-		_config.sigh(_config_handler);
-		_env.parent().announce(_env.ep().manage(_root));
+		_config_rom.sigh(_config_handler);
+		_handle_config();
+		_common.announce_service();
 	}
 };
 
 
 void Driver::Main::_handle_config()
 {
-	_config.update();
-	_devices.update(_config.xml());
-	_root.update_policy();
+	_config_rom.update();
+	_common.handle_config(_config_rom.xml());
 }
 
 
-void Component::construct(Genode::Env &env) { static Driver::Main main(env); }
+void Component::construct(Genode::Env &env) {
+	static Driver::Main main(env); }
