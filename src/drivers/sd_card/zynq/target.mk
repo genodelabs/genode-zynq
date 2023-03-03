37da6b9c858da07356ae4d3e81f394ac2e97ebbc
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2023-03-03 15:23:12 +0100

    lx_emul: adjust and unify time handling
    Ref genodelabs/genode#4778

--- src/drivers/sd_card/zynq/target.mk
+++ src/drivers/sd_card/zynq/target.mk
@@ -12,8 +12,5 @@ SRC_CC  += genode_c_api/block.cc
 GENODE_C_API_SRC_DIR := $(call select_from_repositories,src/lib/genode_c_api)
 vpath genode_c_api/block.cc $(dir $(GENODE_C_API_SRC_DIR))
 
-SRC_CC  += lx_emul/zynq/time.cc
-vpath lx_emul/zynq/time.cc $(REP_DIR)/src/lib
-
 SRC_C  += lx_emul/zynq/common_dummies.c
 vpath lx_emul/zynq/common_dummies.c $(REP_DIR)/src/lib
